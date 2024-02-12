2a630b4ee1fc125c731c1bf0aafa4d1af3c3514e
Imran Hasan <imran.h94@gmail.com>
2024-02-12 16:26:56 +0500

    added a post route for event creation and createEvent fn in main.go

--- models/event.go
+++ models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string	`binding:"required"` // gin tags to enforce key presence in post body
+	Description string	`binding:"required"`
+	Location    string	`binding:"required"`
+	DateTime    time.Time	`binding:"required"`
 	UserID      int
 }
 
