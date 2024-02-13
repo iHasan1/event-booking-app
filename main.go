602dfdacf48b098311b41995291979c00307fbf3
Imran Hasan <imran.h94@gmail.com>
2024-02-13 13:42:18 +0500

    added get single event logic

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+
 	"example.com/event-booking-app/db"
 	"example.com/event-booking-app/models"
 	"github.com/gin-gonic/gin"
@@ -12,14 +14,39 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) 
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events."})
+		return
+	}
+
 	context.JSON(http.StatusOK, events) // this back the response
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
@@ -27,14 +54,18 @@ func createEvent(context *gin.Context) {
 	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"}) // gin.H{} allows us to send a custom map back as response
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."}) // gin.H{} allows us to send a custom map back as response
 		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "data": event})
 }
--- models/event.go
+++ models/event.go
@@ -23,7 +23,7 @@ func (e Event) Save() error {
 	INSERT into events (name, description, location, dateTime, user_id) 
 	VALUES (?, ?, ?, ?, ?)`
 
-	stmt, err := db.DB.Prepare(query)
+	stmt, err := db.DB.Prepare(query) // stores the query in memory to be usable over and over with different values in a highly efficient way
 	if err != nil {
 		return err
 	}
@@ -41,6 +41,41 @@ func (e Event) Save() error {
 	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query) //Prepare() could have been used here as well but was not intentionally - Used query instead of Exec because its just fetching data instead of modifying a record in the database
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+	return events, nil
+}
+
+func GetEventByID(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err // null value for pointer is nil hence the return type is a pointer for this function so that we can use nil here
+	}
+
+	return &event, nil
 }
