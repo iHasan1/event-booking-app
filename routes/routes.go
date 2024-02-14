8e93256f1ae5f2c702d912dfb2b01d5cca0ea2b2
Imran Hasan <imran.h94@gmail.com>
2024-02-14 12:04:04 +0500

    added authentication on update, delete and create routes through middleware grouping

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,22 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/event-booking-app/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
 
+	// middlewares can be attached to grouped routes like this
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", middlewares.Authenticate, createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
+	// server.POST("/events", middlewares.Authenticate, createEvent) // middlewares can also be attached individually like this to routes
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
