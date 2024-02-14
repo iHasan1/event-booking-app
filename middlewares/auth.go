e2d13569e78cb89bd36b68bcabba8cb923bd2b11
Imran Hasan <imran.h94@gmail.com>
2024-02-14 11:52:41 +0500

    added an authentication middleware

---
+++ middlewares/auth.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"example.com/event-booking-app/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"}) // AbortWithStatusJSON stops other middlewares from running
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	context.Next()
+}
